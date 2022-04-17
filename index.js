6f9f65fa6bc2b37aea60d673f20712bbdaa5a3d7
Vunbo MacMini <vunbo@outlook.com>
2022-04-17 16:29:24 +0800

    feat(init): eslint集成配置初始化

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: '@antfu',
+  rules: {
+    // 强制模块大括号换行规则
+    '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    // 模块必须使用大括号
+    'curly': ['error', 'all'],
+  },
+}
