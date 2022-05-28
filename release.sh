6548a7eef9ca8bfd5578a7876afddfc208246026
Vunbo MacMini <vunbo@outlook.com>
2022-05-28 13:42:35 +0800

    [build] 1.0.1

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/evn sh
+#!/usr/bin/env sh
 set -e
 echo "Enter release version: "
-read version
+read VERSION
 read -p "Releasing $VERSION - are you sure ? (y/n)" -n 1 -r
 echo # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
